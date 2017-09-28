97aa54ec68d2a5fb651bf3401691d2ae4abc9259
Bradley <scharfb@umich.edu>
2017-09-28 16:00:58 -0400

    added new code

--- W4Lab.py
+++ W4Lab.py
@@ -1,8 +1,11 @@
-fname = "mbox-short.txt"
-f = open(fname, "r")
-for line in f.readlines():
-	if "From" in line:
-		print (line)
-		name = line.split()[1]
-		n = name.split("@")[0]
-		print (n)
+import re
+
+with open("mbox-short.txt", "r") as f: 
+	f = f.readlines()
+	for line in f:
+		if re.search('From', line):
+			print (line)
+			numbers = re.findall('[0-9]+', line)
+			print (numbers)
+			name = re.findall('(\S*)@', line)
+			print (name)
